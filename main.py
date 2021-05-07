8a4ed30cc8cfd1c176764df332a6cd0a014b1e51
caio-farias <caio4095@gmail.com>
2021-05-07 02:45:35 -0300

    add tweet verse feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 import random
 from time import sleep
+import os
 
 def get_random_phrase(file):
     f = open(file, "r", encoding="utf8")
@@ -10,6 +11,19 @@ def get_random_phrase(file):
     while(len(phrase) == 0 or len(phrase) > 280):
         phrase = random.choice(phrases) + "."
     return phrase
+
+def get_random_verse():
+    albuns = os.listdir('./lyrics')
+    path = './lyrics/' + random.choice(albuns)
+    track = random.choice(os.listdir(path))
+    final_path = path + '/' + track
+    f = open(final_path, "r", encoding="utf8")
+    verses = f.read().split("-")
+    f.close()
+    verse = random.choice(verses)
+    verse += "\n" + "Compositor: Sebastião Maia" 
+    return verse
+
 
 def auth(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
     auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
@@ -32,7 +46,11 @@ ACCESS_TOKEN_SECRET = token[3]
 api = auth(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 count = 0
 while(True):
-    content = get_random_phrase("output.txt")
+    choice = random.randint(0,1)
+    if(choice > 0):
+        content = get_random_phrase("output.txt")
+    else:
+        content = get_random_verse()
     tweet(api, content)
     count += 1
     print(f"Tweet number {count}!")
